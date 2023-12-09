c6a76c34727c346ccdc005b77f91a0eaa02d65f4
rojakatuwal <122533089+rojakatuwal@users.noreply.github.com>
2023-12-08 20:49:19 -0500

    Add files via upload

---
+++ Html Micro site/SCRIPT/script.js
@@ -0,0 +1,15 @@
+async
+      src="https://www.googletagmanager.com/gtag/js?id=G-0K1Z5D0VV1"
+      
+
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+
+      gtag("config", "G-0K1Z5D0VV1");
+
+      
+     
+    
